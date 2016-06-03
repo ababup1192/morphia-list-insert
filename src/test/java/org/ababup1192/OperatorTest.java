2d09652c52f9fdb5c0f8a1253a67b149065ae253
ababup1192 <ababup1192@gmail.com>
2016-06-03 12:53:57 +0900

    Modify list insert

--- src/test/java/org/ababup1192/OperatorTest.java
+++ src/test/java/org/ababup1192/OperatorTest.java
@@ -8,6 +8,7 @@ import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -50,44 +51,28 @@ public class OperatorTest {
         final Datastore datastore = morphia.createDatastore(new MongoClient(), DATA_STORE_NAME);
         // 変更の対象となるのは、idが1であるListEntity
         final Query<ListEntity> selectQuery = datastore.createQuery(ListEntity.class).filter("id", 1);
+        final Query<ListEntity> updatedQuery = datastore.createQuery(ListEntity.class).filter("id", 2);
 
-        // 変更前のListEntityを取得
-        final ListEntity beforeListEntities = selectQuery.asList().get(0);
-        // 削除(disableフラグをtrue)予定のEntityを取得し、クローン
-        EmbeddedEntity lastEmbeddedEntity = beforeListEntities.embeddedEntities.
-                get(beforeListEntities.embeddedEntities.size() - 1).clone();
+        ListEntity updateEntity = updatedQuery.asList().get(0);
 
-        // removeLastメソッドでEntityのembeddedEntriesの最後の要素を削除(するためのオペレーションを用意)
         UpdateOperations<ListEntity> removeOperation = datastore.
-                createUpdateOperations(ListEntity.class).removeLast("embeddedEntities");
+                createUpdateOperations(ListEntity.class).set("embeddedEntities", new ArrayList<>());
 
-        // embeddedEntriesの最後の要素を削除
-        datastore.update(selectQuery, removeOperation);
-
-        // embeddedEntriesの最後の要素の削除フラグをtrueにするために、cloneしておいた、Entityを用意
-        lastEmbeddedEntity.disable = true;
         // 要素追加のオペレーションを用意
-        UpdateOperations<ListEntity> disableOperation = datastore.
-                createUpdateOperations(ListEntity.class).add("embeddedEntities", lastEmbeddedEntity);
+        UpdateOperations<ListEntity> insertOperation = datastore.
+                createUpdateOperations(ListEntity.class).addAll("embeddedEntities", updateEntity.embeddedEntities, true);
 
         // 削除フラグが立っているEmbeddedEntityをinsert
-        datastore.update(selectQuery, disableOperation);
-
-        // 追加予定のEntityのinsert準備
-        UpdateOperations<ListEntity> insertOperation = datastore.
-                createUpdateOperations(ListEntity.class).add("embeddedEntities", new EmbeddedEntity(1004, "text4", false));
-
-        // insert
+        datastore.update(selectQuery, removeOperation);
         datastore.update(selectQuery, insertOperation);
 
         // 比較テスト
         final List<ListEntity> listEntities = selectQuery.asList();
 
         assertThat(listEntities.get(0), is(new ListEntity(1, Arrays.asList(
-                new EmbeddedEntity(1001, "text", false),
-                new EmbeddedEntity(1002, "text2", false),
-                new EmbeddedEntity(1003, "text3", true),
-                new EmbeddedEntity(1004, "text4", false)
+                new EmbeddedEntity(2001, "text", false),
+                new EmbeddedEntity(2002, "text2", false),
+                new EmbeddedEntity(2003, "text3", false)
         ))));
     }
 }
